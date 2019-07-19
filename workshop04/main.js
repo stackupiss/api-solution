fa02fe7a780880d424c098a998151a14a5e3ccfc
Chuk <chukmunnlee@gmail.com>
2019-07-19 13:53:40 +0800

    workshop04

--- workshop04/main.js
+++ workshop04/main.js
@@ -20,7 +20,7 @@ const express = require('express')
 const CitiesDB = require('./citiesdb');
 
 const serviceId = uuid().substring(0, 8);
-const serviceName = `zips-${serviceId}`
+const serviceName = `zips`
 
 //Load application keys
 //Rename _keys.json file to keys.json
@@ -49,13 +49,153 @@ app.use(express.urlencoded({ extended: true }));
 
 // TODO 1/3 Load schemans
 
+const citySchema = require('./schema/city-schema.json');
 
+new OpenAPIValidator({ 
+    apiSpecPath: join(__dirname, 'schema', 'city-api.yaml')
+}).install(app)
 
 
 // TODO 2/3 Copy your routes from workshop03 here
 
+// TODO GET /api/states
+const precondOptions = {
+	stateAsync: (req) => 
+		new Promise((resolve, reject) => {
+			// Returns either the last modified date of the resource
+			// or the latest ETag
+			resolve('"SomeETag"')
+		})
+}
+
+app.get('/api/states', 
+	cacheControl({ maxAge: 30, private: false }),
+	preconditions(precondOptions),
+	(req, resp) => {
+
+	resp.type('application/json')
+
+	db.findAllStates()
+		.then(result => {
+			resp.status(200).json(result)
+		})
+		.catch(error => {
+			resp.status(400).json({ error: error });
+		})
+})
 
 
+// TODO GET /api/state/:state
+app.get('/api/state/:state', 
+	range({ accept: 'items', limit: 20 }),
+	compression(),
+	(req, resp) => {
+
+		const offset = req.range.first;
+		const limit = (req.range.last - req.range.first) + 1
+
+		resp.type('application/json')
+
+		Promise.all([ 
+				db.findCitiesByName(req.params.state, { offset: offset, limit: limit }),
+				db.countCitiesInState(req.params.state) ])
+			.then(result => {
+				resp.status(206)
+				resp.set('Accept-Ranges', 'items')
+				resp.range({
+					first: req.range.first,
+					last: req.range.last,
+					length: result[1]
+				})
+				resp.json(result[0])
+			})
+			.catch(error => {
+				resp.status(400).json({ error: error });
+			})
+	}
+);
+
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:cityId', (req, resp) => {
+
+	resp.type('application/json')
+
+	db.findCityById(req.params.cityId)
+		.then(result => {
+			if (result.length > 0) 
+				return resp.status(200).json(result)
+			resp.status(404).json({ error: `City '${req.params.cityId}' not found` })
+		})
+		.catch(error => {
+			resp.status(400).type('application/json').json({ error: error });
+		})
+})
+
+
+// TODO POST /api/city
+app.post('/api/city', (req, resp) => {
+
+	const params = {
+		city: req.body.city,
+		loc: req.body.loc.map(v => parseFloat(v)),
+		pop: parseInt(req.body.pop),
+		state: req.body.state
+	}
+
+	resp.type('application/json')
+
+	db.insertCity(params)
+		.then(result => {
+			resp.status(201).json(result.ops[0])
+		})
+		.catch(error => {
+			console.error(error);
+			resp.status(400).json({ error: error });
+		})
+});
+
+// Optional workshop
+// TODO HEAD /api/state/:state
+app.head('/api/state/:state', (req, resp) => {
+	resp.type('application/json')
+		.set('Accept-Ranges', 'items')
+		.set('Accept-Encoding', 'gzip')
+		.end()
+})
+
+// TODO GET /state/:state/count
+app.get('/api/state/:state/count', (req, resp) => {
+
+	resp.type('application/json')
+
+	db.countCitiesInState(req.params.state)
+		.then(result => {
+			resp.status(200)
+				.json({
+					state: req.params.state.toUpperCase(),
+					cities: result
+				})
+		})
+		.catch(error => {
+			resp.status(400).json({ error: error });
+		})
+})
+
+// TODO GET /city/:name
+app.get('/api/city/:name', (req, resp) => {
+
+	resp.type('application/json')
+
+	db.findCitiesByName(req.params.cityId)
+		.then(result => {
+			resp.status(200)
+				.json(result)
+		})
+		.catch(error => {
+			resp.status(400).json({ error: error });
+		})
+})
 
 
 // End of workshop
@@ -87,9 +227,27 @@ db.getDB()
 			console.info(`\tService id: ${serviceId}`);
 
 			// TODO 3/3 Add service registration here
-
-
-
+			console.info(`Registering service ${serviceName}:${serviceId}`)
+			consul.agent.service.register({
+				id: serviceId,
+				name: serviceName,
+				port: PORT,
+				check: {
+					ttl: '10s',
+					deregistercriticalserviceafter: '30s'
+				}
+			}).then(() => {
+				setInterval(
+					() => {
+						consul.agent.check.pass({ id: `service:${serviceId}` })
+					}, 10000 //10s
+				)
+				process.on('SIGINT', () => {
+					console.info(`Deregistering service ${serviceName}:${serviceId}`)
+					consul.agent.service.deregister({ id: serviceId })
+						.finally(() => process.exit())
+				})
+			}).catch(error => console.error('Error: ', error))
 
 		});
 	})
